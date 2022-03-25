77b289a4212ffe4ceea61bebdae81502bc42c948
Aureu <honzawaldhaus@gmail.com>
2022-03-25 09:28:43 +0100

    Update profile.js

--- models/profile.js
+++ models/profile.js
@@ -1,2 +1,16 @@
 const conn = require('../database');
 const express = require('express');
+
+exports.getUser = (ID) => {
+	return new Promise((resolve, reject) => {
+		try {
+			let sql = `SELECT * FROM users WHERE user_id = ?`;
+			conn.query(sql, ID, (error, results) => {
+				if (error) throw error;
+				resolve(results);
+			});
+		} catch (err) {
+			reject(err);
+		}
+	});
+};
